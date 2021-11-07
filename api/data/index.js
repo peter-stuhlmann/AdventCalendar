4e34622c5a02c922dfbea2e3c1d2c83f0b3d9197
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-11-07 21:46:26 +0100

    Initialize API

---
+++ api/data/index.js
@@ -0,0 +1,3 @@
+const days = require('./days');
+
+module.exports = days;
---
+++ api/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+const days = require('./data');
+
+app.get('/', (req, res) => {
+  const date = new Date();
+
+  const output = days.map((day) => {
+    const allowed = date.getMonth() === 11 && date.getDate() >= day.day;
+
+    return {
+      day: day.day,
+      allowed: allowed,
+      message: allowed && day.message,
+    };
+  });
+
+  res.status(200).send(output);
+});
+
+app.all('*', (req, res) => {
+  res
+    .status(404)
+    .send(`Error 404: Can't find ${req.originalUrl} on this server!`);
+});
+
+app.listen(port, () => console.log(`API listening on port ${port}`));
