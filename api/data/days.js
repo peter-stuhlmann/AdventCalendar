938c904ce5c6e3c4445b56a9952321b5e3a025c1
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-11-27 05:42:58 +0100

    Add API dummy data

--- api/data/days.js
+++ api/data/days.js
@@ -1,26 +1,125 @@
-module.exports = [
-  { day: 1, message: 'Day I' },
-  { day: 2, message: 'Day II' },
-  { day: 3, message: 'Day III' },
-  { day: 4, message: 'Day IV' },
-  { day: 5, message: 'Day V' },
-  { day: 6, message: 'Day VI' },
-  { day: 7, message: 'Day VII' },
-  { day: 8, message: 'Day VIII' },
-  { day: 9, message: 'Day IX' },
-  { day: 10, message: 'Day X' },
-  { day: 11, message: 'Day XI' },
-  { day: 12, message: 'Day XII' },
-  { day: 13, message: 'Day XIII' },
-  { day: 14, message: 'Day XIV' },
-  { day: 15, message: 'Day XV' },
-  { day: 16, message: 'Day XVI' },
-  { day: 17, message: 'Day XVII' },
-  { day: 18, message: 'Day XVIII' },
-  { day: 19, message: 'Day XIX' },
-  { day: 20, message: 'Day XX' },
-  { day: 21, message: 'Day XXI' },
-  { day: 22, message: 'Day XXII' },
-  { day: 23, message: 'Day XXIII' },
-  { day: 24, message: 'Day XXIV' },
-];
+module.exports = {
+  defaultImage: '',
+  days: [
+    {
+      day: 1,
+      message: 'Day I',
+      img: '',
+    },
+    {
+      day: 2,
+      message: 'Day II',
+      img: '',
+    },
+    {
+      day: 3,
+      message: 'Day III',
+      img: '',
+    },
+    {
+      day: 4,
+      message: 'Day IV',
+      img: '',
+    },
+    {
+      day: 5,
+      message: 'Day V',
+      img: '',
+    },
+    {
+      day: 6,
+      message: 'Day VI',
+      img: '',
+    },
+    {
+      day: 7,
+      message: 'Day VII',
+      img: '',
+    },
+    {
+      day: 8,
+      message: 'Day VIII',
+      img: '',
+    },
+    {
+      day: 9,
+      message: 'Day IX',
+      img: '',
+    },
+    {
+      day: 10,
+      message: 'Day X',
+      img: '',
+    },
+    {
+      day: 11,
+      message: 'Day XI',
+      img: '',
+    },
+    {
+      day: 12,
+      message: 'Day XII',
+      img: '',
+    },
+    {
+      day: 13,
+      message: 'Day XIII',
+      img: '',
+    },
+    {
+      day: 14,
+      message: 'Day XIV',
+      img: '',
+    },
+    {
+      day: 15,
+      message: 'Day XV',
+      img: '',
+    },
+    {
+      day: 16,
+      message: 'Day XVI',
+      img: '',
+    },
+    {
+      day: 17,
+      message: 'Day XVII',
+      img: '',
+    },
+    {
+      day: 18,
+      message: 'Day XVIII',
+      img: '',
+    },
+    {
+      day: 19,
+      message: 'Day XIX',
+      img: '',
+    },
+    {
+      day: 20,
+      message: 'Day XX',
+      img: '',
+    },
+    {
+      day: 21,
+      message: 'Day XXI',
+      img: '',
+    },
+    {
+      day: 22,
+      message: 'Day XXII',
+      img: '',
+    },
+    {
+      day: 23,
+      message: 'Day XXIII',
+      img: '',
+    },
+    {
+      day: 24,
+      message: 'Day XXIV',
+      img: '',
+    },
+  ],
+};
