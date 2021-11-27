4cf2f79269df1b364b3bc3f41345fce0f92128b7
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-11-27 05:24:11 +0100

    Change response structure

--- api/index.js
+++ api/index.js
@@ -3,20 +3,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-const days = require('./data');
+const data = require('./data');
 
 app.get('/api', (req, res) => {
   const date = new Date();
 
-  const output = days.map((day) => {
+  const days = data.days.map((day) => {
     const allowed = date.getMonth() === 11 && date.getDate() >= day.day;
 
     return {
       day: day.day,
       allowed: allowed,
       message: allowed && day.message,
+      img: allowed && day.img,
     };
   });
+
+  const output = { defaultImage: data.defaultImage, days };
 
   res.status(200).send(output);
 });
